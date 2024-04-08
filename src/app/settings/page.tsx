7427f353613ceae3c5d3ce13c47b7273d99ce296
404salad <404salad@gmail.com>
2024-04-08 11:22:51 +0530

    Created page for notification

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -4,9 +4,9 @@ import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 export const metadata: Metadata = {
-  title: "Next.js Settings | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Settings page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+  title:
+    "Hostel Admin Dashboard | HostelAdmin - Administration for the hostel",
+  description: "This is website for Admin Dashboard",
 };
 
 const Settings = () => {
--- src/app/tables/page.tsx
+++ src/app/tables/page.tsx
@@ -7,9 +7,9 @@ import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 export const metadata: Metadata = {
-  title: "Next.js Tables | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Tables page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+  title:
+    "Hostel Admin Dashboard | HostelAdmin - Administration for the hostel",
+  description: "This is website for Admin Dashboard",
 };
 
 const TablesPage = () => {
